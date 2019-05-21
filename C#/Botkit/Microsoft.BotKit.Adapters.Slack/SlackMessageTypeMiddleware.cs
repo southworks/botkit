942f55d36bac747b6e000702edae39374b2f73b9
matiasroldan6 <matias.roldan@southworks.com>
2019-05-21 12:46:40 -0300

    Add renaming of botbuilder_slack_adapter namespace and project

--- C#/Botkit/Microsoft.BotKit.Adapters.Slack/SlackMessageTypeMiddleware.cs
+++ C#/Botkit/Microsoft.BotKit.Adapters.Slack/SlackMessageTypeMiddleware.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace botbuilder_slack_adapter
+namespace Microsoft.BotKit.Adapters.Slack
 {
     /// <summary>
     /// A middleware for Botkit developers using the BotBuilder SlackAdapter class.
